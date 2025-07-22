12365df3b18019a3c4b924e0ec1c5808daf9821b
hiuth <hiuth2004@gmail.com>
2025-07-22 16:59:03 +0700

    Format codebase for consistent indentation and update `pom.xml` with new compiler settings, dependencies, and plugins. Add application configuration in `application.yaml`.

--- src/main/java/org/example/gundokai/GunDoKaiApplication.java
+++ src/main/java/org/example/gundokai/GunDoKaiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GunDoKaiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GunDoKaiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GunDoKaiApplication.class, args);
+    }
 
 }
