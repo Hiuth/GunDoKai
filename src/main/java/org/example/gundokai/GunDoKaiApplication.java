db3f2ecbd96dc130d7c3c3a84d7b71682cf7abac
hiuth <hiuth2004@gmail.com>
2025-07-22 16:58:47 +0700

    Initial commit

---
+++ src/main/java/org/example/gundokai/GunDoKaiApplication.java
@@ -0,0 +1,13 @@
+package org.example.gundokai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunDoKaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunDoKaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/gundokai/GunDoKaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gundokai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunDoKaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
